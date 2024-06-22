9f172f20f33c46d4454e00d11b05ae692b1d29c6
harshavardhanck123 <ckharshavardhan00@gmail.com>
2024-06-22 19:43:52 +0530

    changed auth in search

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/',auth.checkAuth,eventController.create)
 router.get('/',auth.checkAuth,eventController.getAllEvents)
-router.get('/search',auth.checkAuth, eventController.searchEvents);
+router.get('/search',eventController.searchEvents);
 router.get('/:id',auth.checkAuth,eventController.getEvent)
 router.put('/:id',auth.checkAuth,auth.isAdmin,eventController.updateEvent)
 router.delete("/:id",auth.checkAuth,auth.isAdmin,eventController.deleteEvent)
